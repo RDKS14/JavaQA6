981adcfccb027d28e66a12374a313a6a961fd9e2
RDKS14 <rob.smith7@hotmail.co.uk>
2023-09-08 14:12:36 +0100

    variables

--- src/runner/Runner.java
+++ src/runner/Runner.java
@@ -7,4 +7,3 @@ public class Runner {
 
     }
 }
-cd
---
+++ src/runner/variables.java
@@ -0,0 +1,31 @@
+package runner;
+
+public class variables {
+
+
+    public static void main(String[] args) {
+
+        //      String firstName = "Rob ";
+        //       String lastName = "Smith";
+        //       String name = (firstName + lastName);
+
+        //       System.out.println(name);
+
+        float num1 = 1;
+        int num2 = 3;
+
+        System.out.println(num1 + num2);
+
+        float result = (num1 / num2);
+
+        System.out.println(result);
+
+        int num3 = 8;
+
+        System.out.println(num3++);
+        System.out.println(num3);
+        System.out.println(++num3);
+
+    }
+}
+
